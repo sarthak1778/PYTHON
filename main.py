a2fcfc4c9b4df2184b41594d09c8ccc07a0d45d6
sarthak1778 <sarthakchoudhary1778@gmail.com>
2026-05-12 03:16:52 +0530

    PYTHON

---
+++ main.py
@@ -0,0 +1,23 @@
+print("Hello World");
+print("A+B");
+print(5);
+#this is use to add the single line comment in the program.
+#ctrl+/ is use to comment and un-comment multiple lines at a same time by selecting all those lines.
+print("output") #will  print 
+'''
+to move any line in the multi-level;
+we can use the triple-single quotes 
+or triple-double quotes
+
+and to move any line up in the VS Code
+use shortcut Alt+ arrow.......
+'''
+print("My name is \"Sarthak\"\nand my surname is Choudhary.");
+print("Hey",6,7);
+print("Hey",6,7,sep="-",end="009")
+print("Hello")
+print("Hey",6,7,sep="-",end="009\n")
+print("Hello")
+
+
+
