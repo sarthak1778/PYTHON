5f9853c4bc8bdaa655a90a4b395ad3bc64c6ae52
sarthak1778 <sarthakchoudhary1778@gmail.com>
2026-08-07 00:34:05 +0530

    after solving few problems in python I have learned about the factorial and fibonacci numbers. I have created a new file for factorial and modified the fibonacci numbers file. I have also deleted the array.py file as it was not needed anymore.

--- array.py
+++
@@ -1,7 +0,0 @@
-"""
-1. Create an array with 5 values and delete the value at index 2 without using the built-in `del` function. 
-2.WAP to reverse an array without using the built-in `reverse()` function.
-"""
-import numpy as np
-x=np.array([10, 20, 30, 40, 50])
-print("Original array:", x)
